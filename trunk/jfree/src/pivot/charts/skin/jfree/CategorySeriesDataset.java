04f4314ad11858bdce8d9e3d1a3677ccd8f09732
gkbrown.mac <gkbrown.mac@454b6a07-811e-684a-823d-e3eb653d8f17>
2009-04-17 12:39:14 +0000

    Rename pivot.charts.skin to pivot.charts.skin.jfree.

--- trunk/jfree/src/pivot/charts/skin/jfree/CategorySeriesDataset.java
+++ trunk/jfree/src/pivot/charts/skin/jfree/CategorySeriesDataset.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package pivot.charts.skin;
+package pivot.charts.skin.jfree;
 
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.general.DatasetChangeListener;
