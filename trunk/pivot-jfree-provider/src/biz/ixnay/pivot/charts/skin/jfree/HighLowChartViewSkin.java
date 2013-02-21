c5d6ebe16f37571f2cae4a01377cac8e785d7007
sandro.martini <sandro.martini@454b6a07-811e-684a-823d-e3eb653d8f17>
2013-02-21 16:44:54 +0000

    Pivot-JFree, initial work for release 2.0.3 (soon)

---
+++ trunk/pivot-jfree-provider/src/biz/ixnay/pivot/charts/skin/jfree/HighLowChartViewSkin.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2009 VMware, Inc.
+ * Copyright 2009 G. Brown, ixnay.biz.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package biz.ixnay.pivot.charts.skin.jfree;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.entity.ChartEntity;
+import org.jfree.chart.entity.XYItemEntity;
+
+import org.apache.pivot.charts.ChartView;
+import org.apache.pivot.charts.HighLowChartView;
+import org.apache.pivot.collections.List;
+
+/**
+ * High/low chart view skin.
+ *
+ * @author gbrown
+ */
+public class HighLowChartViewSkin extends JFreeChartViewSkin {
+    private boolean candlestick = false;
+
+    @Override
+	public ChartView.Element getElementAt(int x, int y) {
+        ChartView.Element element = null;
+
+        ChartEntity chartEntity = getChartEntityAt(x, y);
+        if (chartEntity instanceof XYItemEntity) {
+            XYItemEntity xyItemEntity = (XYItemEntity)chartEntity;
+            element = new ChartView.Element(xyItemEntity.getSeriesIndex(),
+                xyItemEntity.getItem());
+        }
+
+        return element;
+    }
+
+    @Override
+    protected JFreeChart createChart() {
+        HighLowChartView chartView = (HighLowChartView)getComponent();
+
+        String title = chartView.getTitle();
+        String horizontalAxisLabel = chartView.getHorizontalAxisLabel();
+        String verticalAxisLabel = chartView.getVerticalAxisLabel();
+        boolean showLegend = chartView.getShowLegend();
+
+        String seriesNameKey = chartView.getSeriesNameKey();
+        List<?> chartData = chartView.getChartData();
+
+        JFreeChart chart;
+        OHLCSeriesDataset dataset = new OHLCSeriesDataset(seriesNameKey, chartData);
+
+        if (candlestick) {
+            chart = ChartFactory.createCandlestickChart(title,
+                horizontalAxisLabel, verticalAxisLabel, dataset, showLegend);
+        } else {
+            chart = ChartFactory.createHighLowChart(title,
+                horizontalAxisLabel, verticalAxisLabel, dataset, showLegend);
+        }
+
+        return chart;
+    }
+
+    public boolean isCandlestick() {
+        return candlestick;
+    }
+
+    public void setCandlestick(boolean candlestick) {
+        this.candlestick = candlestick;
+        repaintComponent();
+    }
+}
