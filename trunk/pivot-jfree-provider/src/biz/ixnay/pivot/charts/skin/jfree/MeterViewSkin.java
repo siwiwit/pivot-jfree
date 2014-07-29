7e72ebb58abeace694c6f6419d849e0146d35b4d
roger.whitcomb3@gmail.com <roger.whitcomb3@gmail.com@454b6a07-811e-684a-823d-e3eb653d8f17>
2014-07-29 18:01:19 +0000

    Adding missing MeterViewSkin.java file (referenced already in JFreeChartProvider).
    This version of the code is only a skeleton, meant to simply satisfy the compiler.

---
+++ trunk/pivot-jfree-provider/src/biz/ixnay/pivot/charts/skin/jfree/MeterViewSkin.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package biz.ixnay.pivot.charts.skin.jfree;
+
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.entity.ChartEntity;
+import org.jfree.chart.plot.MeterPlot;
+import org.jfree.data.general.ValueDataset;
+
+import org.apache.pivot.charts.SingleValueChartView;
+import org.apache.pivot.charts.MeterView;
+
+/**
+ * Meter chart view skin.
+ *
+ * @author rwhitcomb
+ */
+public class MeterViewSkin extends SingleValueChartSkin {
+
+    @Override
+    protected JFreeChart createChart() {
+        // TODO: implement this chart
+        return null;
+    }
+
+}
